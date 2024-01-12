f4d5d8fa678dfe33353a9b1f61b1792adf41907d
csct3434 <csct3434@gmail.com>
2024-01-13 01:06:01 +0900

    feat: KakaoLoginService - 리프레시 토큰 발급 기능 추가

--- src/main/java/today/seasoning/seasoning/user/service/kakao/KakaoLoginService.java
+++ src/main/java/today/seasoning/seasoning/user/service/kakao/KakaoLoginService.java
@@ -8,10 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import today.seasoning.seasoning.common.enums.LoginType;
+import today.seasoning.seasoning.common.token.domain.TokenInfo;
+import today.seasoning.seasoning.common.token.service.RefreshTokenService;
 import today.seasoning.seasoning.common.util.JwtUtil;
 import today.seasoning.seasoning.user.domain.User;
 import today.seasoning.seasoning.user.domain.UserRepository;
-import today.seasoning.seasoning.user.dto.LoginResultDto;
+import today.seasoning.seasoning.user.dto.LoginResult;
 import today.seasoning.seasoning.user.dto.SocialUserProfileDto;
 
 @Slf4j
@@ -23,43 +25,42 @@ public class KakaoLoginService {
 
     private final ExchangeKakaoAccessToken exchangeKakaoAccessToken;
     private final FetchKakaoUserProfile fetchKakaoUserProfile;
+    private final RefreshTokenService refreshTokenService;
     private final UserRepository userRepository;
-    private final JwtUtil jwtUtil;
 
     @Transactional
-    public LoginResultDto handleKakaoLogin(String authorizationCode) {
+    public LoginResult handleKakaoLogin(String authorizationCode) {
         // 액세스 토큰 교환
         String accessToken = exchangeKakaoAccessToken.doExchange(authorizationCode);
 
         // 카카오 프로필 조회
         SocialUserProfileDto userProfile = fetchKakaoUserProfile.doFetch(accessToken);
 
-        // 계정 조회 (신규 유저인 경우 가입 처리)
-        UserInfo userInfo = registerUserIfNeeded(userProfile);
+        // 로그인 처리 (신규 유저인 경우 회원 등록)
+        LoginInfo loginInfo = handleLogin(userProfile);
 
         // 토큰 발급
-        String token = createToken(userInfo.getUser());
+        TokenInfo tokenInfo = JwtUtil.createToken(loginInfo.getUser().getId(), KAKAO_LOGIN_TYPE);
 
-        return new LoginResultDto(token, userInfo.isFirstLogin());
+        // 리프레시 토큰 저장
+        refreshTokenService.save(tokenInfo.getRefreshToken(), loginInfo.getUser().getId());
+
+        return new LoginResult(tokenInfo, loginInfo.isFirstLogin());
     }
 
-    private UserInfo registerUserIfNeeded(SocialUserProfileDto userProfile) {
+    private LoginInfo handleLogin(SocialUserProfileDto userProfile) {
         Optional<User> foundUser = userRepository.find(userProfile.getEmail(), KAKAO_LOGIN_TYPE);
 
         if (foundUser.isEmpty()) {
             User user = userRepository.save(userProfile.toEntity(LoginType.KAKAO));
-            return new UserInfo(user, true);
+            return new LoginInfo(user, true);
         }
-        return new UserInfo(foundUser.get(), false);
-    }
-
-    private String createToken(User user) {
-        return jwtUtil.createToken(user.getId(), KAKAO_LOGIN_TYPE);
+        return new LoginInfo(foundUser.get(), false);
     }
 
     @Getter
     @AllArgsConstructor
-    private static class UserInfo {
+    private static class LoginInfo {
 
         private User user;
         private boolean firstLogin;
