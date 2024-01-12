56972313781986e81e71d69e9b03ef291f3a5d96
csct3434 <csct3434@gmail.com>
2024-01-13 01:33:54 +0900

    refactor: 토큰 전달 방식 변경에 따른 JwtFilter 수정
    - 토큰 전달 방식 변경 : 쿠키 -> Authorization 헤더
    - 토큰 유효성 검증 로직 추가

--- src/main/java/today/seasoning/seasoning/common/config/JwtFilter.java
+++ src/main/java/today/seasoning/seasoning/common/config/JwtFilter.java
@@ -1,22 +1,24 @@
 package today.seasoning.seasoning.common.config;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import today.seasoning.seasoning.common.UserPrincipal;
 import today.seasoning.seasoning.common.exception.CustomException;
@@ -31,49 +33,53 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final UserRepository userRepository;
 
+    private static final Pattern authorizationPattern = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+=*)$",
+        Pattern.CASE_INSENSITIVE);
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-        FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
 
-        Cookie[] cookies = request.getCookies();
-
-        String authorization = null;
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("Authorization")) {
-                    authorization = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        if (authorization == null || !authorization.startsWith("Bearer ")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 누락");
+        String token = resolveTokenFromAuthorizationHeader(request);
+        if (token == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Missing Token");
+            filterChain.doFilter(request, response);
             return;
         }
-
-        String token = authorization.split(" ")[1];
 
         try {
-            UserPrincipal userPrincipal = createPrincipalFromToken(token);
+            UserPrincipal principal = createPrincipal(token);
 
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                userPrincipal, null, List.of(new SimpleGrantedAuthority("USER")));
+            AbstractAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                principal, null, List.of(new SimpleGrantedAuthority("ROLE_USER")));
 
-            authenticationToken.setDetails(
-                new WebAuthenticationDetailsSource().buildDetails(request));
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        } catch (ExpiredJwtException e) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰 만료");
-            return;
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (Exception e) {
             logger.error(e.getMessage());
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "토큰 오류");
-            return;
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Invalid Token");
         }
         filterChain.doFilter(request, response);
+    }
+
+    private String resolveTokenFromAuthorizationHeader(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (!StringUtils.startsWithIgnoreCase(authorization, "bearer")) {
+            return null;
+        }
+        Matcher matcher = authorizationPattern.matcher(authorization);
+        if (!matcher.matches()) {
+            throw new BadCredentialsException("Bearer token is malformed");
+        }
+        return matcher.group("token");
+    }
+
+    private UserPrincipal createPrincipal(String token) {
+        User user = userRepository.findById(JwtUtil.getUserId(token))
+            .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "회원 조회 실패"));
+
+        return UserPrincipal.builder(user);
     }
 
     @Override
@@ -86,12 +92,5 @@ public class JwtFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
 
         return allowedPath.stream().anyMatch(path::equals);
-    }
-
-    private UserPrincipal createPrincipalFromToken(String token) {
-        User user = userRepository.findById(JwtUtil.getUserId(token))
-            .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "회원 조회 실패"));
-
-        return UserPrincipal.builder(user);
     }
 }
