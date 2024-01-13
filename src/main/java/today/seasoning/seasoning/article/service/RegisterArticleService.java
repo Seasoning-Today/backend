ba90aa0cacad62c85da45c3007b6b7d8c9310aba
csct3434 <csct3434@gmail.com>
2024-01-14 01:48:04 +0900

    feat: SolarTerm 서비스 로직 구현
    - 기존 SolarTermUtil 기능을 서비스 로직으로 구현
    - 매년 12월 1일 다음 년도 절기를 DB에 등록하도록 스케줄링 설정
      - 스케줄링 결과를 AWS SNS 토픽으로 전송

--- src/main/java/today/seasoning/seasoning/article/service/RegisterArticleService.java
+++ src/main/java/today/seasoning/seasoning/article/service/RegisterArticleService.java
@@ -1,6 +1,7 @@
 package today.seasoning.seasoning.article.service;
 
 import com.github.f4b6a3.tsid.TsidCreator;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +17,8 @@ import today.seasoning.seasoning.article.dto.RegisterArticleCommand;
 import today.seasoning.seasoning.common.aws.S3Service;
 import today.seasoning.seasoning.common.aws.UploadFileInfo;
 import today.seasoning.seasoning.common.exception.CustomException;
-import today.seasoning.seasoning.common.util.SolarTermUtil;
 import today.seasoning.seasoning.common.util.TsidUtil;
+import today.seasoning.seasoning.solarterm.service.SolarTermService;
 import today.seasoning.seasoning.user.domain.User;
 import today.seasoning.seasoning.user.domain.UserRepository;
 
@@ -26,78 +27,69 @@ import today.seasoning.seasoning.user.domain.UserRepository;
 @RequiredArgsConstructor
 public class RegisterArticleService {
 
-	private final S3Service s3Service;
-	private final UserRepository userRepository;
-	private final ArticleRepository articleRepository;
-	private final ArticleImageRepository articleImageRepository;
+    private final S3Service s3Service;
+    private final UserRepository userRepository;
+    private final SolarTermService solarTermService;
+    private final ArticleRepository articleRepository;
+    private final ArticleImageRepository articleImageRepository;
 
-	@Value("${ARTICLE_IMAGES_LIMIT}")
-	private int ARTICLE_IMAGES_LIMIT;
+    @Value("${ARTICLE_IMAGES_LIMIT}")
+    private int ARTICLE_IMAGES_LIMIT;
 
-	public Long doRegister(RegisterArticleCommand command) {
-		validateRequest(command);
+    public Long doRegister(RegisterArticleCommand command) {
+        validateRequest(command);
 
-		Article article = createArticle(command);
-		articleRepository.save(article);
+        Article article = createArticle(command);
+        articleRepository.save(article);
 
-		uploadAndRegisterArticleImages(article, command.getImages());
+        uploadAndRegisterArticleImages(article, command.getImages());
 
-		return article.getId();
-	}
+        return article.getId();
+    }
 
-	private void validateRequest(RegisterArticleCommand command) {
-		checkSolarTerm();
-		checkArticleImagesLimit(command.getImages());
-	}
+    private void validateRequest(RegisterArticleCommand command) {
+        if (!solarTermService.checkRecordOpen()) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "등록 기간이 아닙니다.");
+        }
 
-	private void checkSolarTerm() {
-		if (SolarTermUtil.getCurrentTerm() == -1) {
-			throw new CustomException(HttpStatus.FORBIDDEN, "등록기간이 아닙니다.");
-		}
-	}
+        if (command.getImages().size() > ARTICLE_IMAGES_LIMIT) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "이미지 개수 초과");
+        }
+    }
 
-	private void checkArticleImagesLimit(List<MultipartFile> images) {
-		if(images.size() > ARTICLE_IMAGES_LIMIT) {
-			throw new CustomException(HttpStatus.FORBIDDEN, "최대 이미지 개수: " + ARTICLE_IMAGES_LIMIT);
-		}
-	}
+    private Article createArticle(RegisterArticleCommand command) {
+        User user = userRepository.findById(command.getUserId()).get();
 
-	private Article createArticle(RegisterArticleCommand command) {
-		User user = userRepository.findById(command.getUserId()).get();
+        return new Article(user, command.isPublished(), LocalDate.now().getYear(),
+            solarTermService.findCurrentTerm(), command.getContents());
+    }
 
-		return new Article(user,
-			command.isPublished(),
-			SolarTermUtil.getCurrentYear(),
-			SolarTermUtil.getCurrentTerm(),
-			command.getContents());
-	}
+    private void uploadAndRegisterArticleImages(Article article, List<MultipartFile> images) {
+        for (int sequence = 0; sequence < images.size(); sequence++) {
+            MultipartFile image = images.get(sequence);
+            UploadFileInfo fileInfo = uploadImage(image);
+            registerArticleImage(article, fileInfo, sequence + 1);
+        }
+    }
 
-	private void uploadAndRegisterArticleImages(Article article, List<MultipartFile> images) {
-		for (int sequence = 0; sequence < images.size(); sequence++) {
-			MultipartFile image = images.get(sequence);
-			UploadFileInfo fileInfo = uploadImage(image);
-			registerArticleImage(article, fileInfo, sequence + 1);
-		}
-	}
+    private UploadFileInfo uploadImage(MultipartFile image) {
+        String uid = TsidCreator.getTsid().encode(62);
+        String originalFilename = image.getOriginalFilename();
+        String uploadFileName = "images/article/" + uid + "/" + originalFilename;
 
-	private UploadFileInfo uploadImage(MultipartFile image) {
-		String uid = TsidCreator.getTsid().encode(62);
-		String originalFilename = image.getOriginalFilename();
-		String uploadFileName = "images/article/" + uid + "/" + originalFilename;
+        String url = s3Service.uploadFile(image, uploadFileName);
 
-		String url = s3Service.uploadFile(image, uploadFileName);
+        return new UploadFileInfo(uploadFileName, url);
+    }
 
-		return new UploadFileInfo(uploadFileName, url);
-	}
+    private void registerArticleImage(Article article, UploadFileInfo fileInfo, int sequence) {
+        ArticleImage articleImage = new ArticleImage(
+            TsidUtil.createLong(),
+            article,
+            fileInfo.getFilename(),
+            fileInfo.getUrl(),
+            sequence);
 
-	private void registerArticleImage(Article article, UploadFileInfo fileInfo, int sequence) {
-		ArticleImage articleImage = new ArticleImage(
-			TsidUtil.createLong(),
-			article,
-			fileInfo.getFilename(),
-			fileInfo.getUrl(),
-			sequence);
-
-		articleImageRepository.save(articleImage);
-	}
+        articleImageRepository.save(articleImage);
+    }
 }
---
+++ src/main/java/today/seasoning/seasoning/solarterm/service/scheduled/RegisterNextYearSolarTermsService.java
@@ -0,0 +1,138 @@
+package today.seasoning.seasoning.solarterm.service.scheduled;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import today.seasoning.seasoning.common.aws.SnsService;
+import today.seasoning.seasoning.solarterm.domain.SolarTerm;
+import today.seasoning.seasoning.solarterm.domain.SolarTermRepository;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RegisterNextYearSolarTermsService {
+
+    @Value("${OPEN_API_KEY}")
+    private String API_KEY;
+
+    private final SnsService snsService;
+    private final SolarTermRepository solarTermRepository;
+
+
+    @Scheduled(cron = "0 0 0 1 12 *")
+    public void findAndRegisterNextYearSolarTerms() {
+        String nextYear = String.valueOf(LocalDate.now().getYear() + 1);
+        List<String> months = List.of("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12");
+
+        try {
+            List<LocalDate> nextYearSolarTermDates = new ArrayList<>(24);
+
+            for (String month : months) {
+                findAndAddSolarTermDate(nextYear, month, nextYearSolarTermDates);
+            }
+
+            for (int i = 0; i < 24; i++) {
+                SolarTerm solarTerm = new SolarTerm(i + 1, nextYearSolarTermDates.get(i));
+                solarTermRepository.save(solarTerm);
+            }
+
+            snsService.publish(nextYear + "년 절기 등록 완료");
+            log.info("{}년 절기 등록 완료", nextYear);
+        } catch (Exception e) {
+            snsService.publish(nextYear + "년 절기 등록 실패 : " + e.getMessage());
+            log.error("{}년 절기 등록 실패 : {}", nextYear, e.getMessage());
+        }
+    }
+
+    private void findAndAddSolarTermDate(String year, String month, List<LocalDate> terms) throws Exception {
+        String xmlString = getXmlString(year, month);
+        parseAndAddTerms(xmlString, terms);
+    }
+
+    private String getXmlString(String year, String month) throws Exception {
+        URL url = buildUrl(year, month);
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+
+        BufferedReader rd = (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) ?
+            new BufferedReader(new InputStreamReader(conn.getInputStream())) :
+            new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        return sb.toString();
+    }
+
+    private void parseAndAddTerms(String xmlString, List<LocalDate> terms) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        ByteArrayInputStream input = new ByteArrayInputStream(
+            xmlString.getBytes(StandardCharsets.UTF_8));
+        Document doc = builder.parse(input);
+        NodeList itemList = doc.getElementsByTagName("item");
+
+        String locdate1 = ((Element) itemList.item(0)).getElementsByTagName("locdate").item(0).getTextContent();
+        String locdate2 = ((Element) itemList.item(1)).getElementsByTagName("locdate").item(0).getTextContent();
+
+        terms.add(LocalDate.parse(locdate1, DateTimeFormatter.ofPattern("yyyyMMdd")));
+        terms.add(LocalDate.parse(locdate2, DateTimeFormatter.ofPattern("yyyyMMdd")));
+    }
+
+    private URL buildUrl(String year, String month) throws MalformedURLException {
+
+        String urlBuilder = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/get24DivisionsInfo"
+
+            // API 키
+            + "?" + URLEncoder.encode("serviceKey", StandardCharsets.UTF_8) + "=" + API_KEY
+
+            // 조회 년도
+            + "&" + URLEncoder.encode("solYear", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(year,
+            StandardCharsets.UTF_8)
+
+            // 조회 월
+            + "&" + URLEncoder.encode("solMonth", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(month,
+            StandardCharsets.UTF_8)
+
+            // 고정값
+            + "&" + URLEncoder.encode("kst", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("0000",
+            StandardCharsets.UTF_8)
+            + "&" + URLEncoder.encode("sunLongitude", StandardCharsets.UTF_8) + "="
+            + URLEncoder.encode("285", StandardCharsets.UTF_8)
+            + "&" + URLEncoder.encode("numOfRows", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(
+            "10", StandardCharsets.UTF_8)
+            + "&" + URLEncoder.encode("pageNo", StandardCharsets.UTF_8) + "=" + URLEncoder.encode("1",
+            StandardCharsets.UTF_8)
+            + "&" + URLEncoder.encode("totalCount", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(
+            "210114", StandardCharsets.UTF_8);
+
+        return new URL(urlBuilder);
+    }
+}
