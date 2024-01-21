d23d2f7eb8b8b3712f97e6d34e5567275656e22e
csct3434 <csct3434@gmail.com>
2024-01-22 02:29:12 +0900

    feat: 아이디 유효성 검증 객체

---
+++ src/main/java/today/seasoning/seasoning/user/domain/AccountId.java
@@ -0,0 +1,34 @@
+package today.seasoning.seasoning.user.domain;
+
+import org.springframework.http.HttpStatus;
+import today.seasoning.seasoning.common.exception.CustomException;
+
+public class AccountId {
+
+    private final String accountId;
+
+    public AccountId(String accountId) {
+        this.accountId = accountId;
+        selfValidate();
+    }
+
+    /*
+    [아이디 규칙]
+    - 5글자 이상, 20글자 이하
+    - 영문 소문자, 숫자, 밑줄('_') 및 점('.')으로 구성
+    - 점으로 시작하거나 끝날 수 없습니다.
+    - 연속된 점은 포함될 수 없습니다.
+    */
+    private void selfValidate() {
+        String upperCases = ".*[A-Z].*";
+        String regex = "^(?!.*\\.\\.)(?!.*\\.$)[^\\W][\\w.]{4,19}$";
+
+        if (accountId == null || accountId.matches(upperCases) || !accountId.matches(regex)) {
+            throw new CustomException(HttpStatus.FORBIDDEN, "Invalid ID Format");
+        }
+    }
+
+    public String get() {
+        return accountId;
+    }
+}
