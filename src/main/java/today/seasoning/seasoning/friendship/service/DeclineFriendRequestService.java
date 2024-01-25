cbede2b919eb93f5ab9319a07fbf2508adbece74
csct3434 <csct3434@gmail.com>
2024-01-26 04:36:58 +0900

    refactor: 친구 요청 거부 서비스 로직 개선
    - FriendRequest를 활용한 서비스 로직 단순화

---
+++ src/main/java/today/seasoning/seasoning/friendship/service/DeclineFriendRequestService.java
@@ -0,0 +1,31 @@
+package today.seasoning.seasoning.friendship.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import today.seasoning.seasoning.common.exception.CustomException;
+import today.seasoning.seasoning.friendship.domain.FriendRequestRepository;
+import today.seasoning.seasoning.user.domain.User;
+import today.seasoning.seasoning.user.domain.UserRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeclineFriendRequestService {
+
+    private final UserRepository userRepository;
+    private final FriendRequestRepository friendRequestRepository;
+
+    public void doService(Long userId, String requesterAccountId) {
+        User requester = userRepository.findByAccountId(requesterAccountId)
+            .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "상대방 조회 실패"));
+
+        if (!friendRequestRepository.existsByFromUserIdAndToUserId(requester.getId(), userId)) {
+            throw new CustomException(HttpStatus.NOT_FOUND, "신청 내역 없음");
+        }
+
+        friendRequestRepository.deleteByFromUserIdAndToUserId(requester.getId(), userId);
+    }
+}
