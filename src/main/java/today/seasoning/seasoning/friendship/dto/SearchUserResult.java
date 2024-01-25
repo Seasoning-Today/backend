ee03c49b5ff0a9aee6edcaca0cae3972bf79a995
csct3434 <csct3434@gmail.com>
2024-01-26 04:55:10 +0900

    refactor: 회원 검색 서비스 로직 개선
    FriendRequest를 활용한 서비스 로직 단순화
    
    - 클래스명 변경 : SearchFriendshipService -> SearchUserService
      - 친구 검색이 아닌, 회원 검색이므로 User가 더 자연스럽다
    - build 메서드를 추가하여 SearchUserResult 객체 생성을 추상화

--- src/main/java/today/seasoning/seasoning/friendship/dto/SearchUserResult.java
+++ src/main/java/today/seasoning/seasoning/friendship/dto/SearchUserResult.java
@@ -3,23 +3,29 @@ package today.seasoning.seasoning.friendship.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import today.seasoning.seasoning.common.enums.FriendshipStatus;
+import today.seasoning.seasoning.user.domain.User;
 
 @Getter
 @RequiredArgsConstructor
-public class SearchFriendResult {
+public class SearchUserResult {
+
     private final String nickname;
     private final String image;
     private final String accountId;
     private FriendshipStatus friendshipStatus;
 
-    public SearchFriendResult(String nickname, String image, String accountId, FriendshipStatus friendshipStatus) {
+    public SearchUserResult(String nickname, String image, String accountId, FriendshipStatus friendshipStatus) {
         this.nickname = nickname;
         this.image = image;
         this.accountId = accountId;
         this.friendshipStatus = friendshipStatus;
     }
 
-    public void setFriendshipStatus(FriendshipStatus friendshipStatus) {
-        this.friendshipStatus = friendshipStatus;
+    public static SearchUserResult build(User friend, FriendshipStatus friendshipStatus) {
+        return new SearchUserResult(
+            friend.getNickname(),
+            friend.getProfileImageUrl(),
+            friend.getAccountId(),
+            friendshipStatus);
     }
 }
