10ff4012a689a9bd886b02bae4eb68f916a3b321
csct3434 <csct3434@gmail.com>
2024-01-26 01:29:48 +0900

    feat: FriendRequest 엔티티 및 리포지토리 작성
    - 친구 신청 내역을 나타내는 엔티티 및 관련 리포지토리 작성

---
+++ src/main/java/today/seasoning/seasoning/friendship/domain/FriendRequest.java
@@ -0,0 +1,39 @@
+package today.seasoning.seasoning.friendship.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import today.seasoning.seasoning.common.BaseTimeEntity;
+import today.seasoning.seasoning.common.util.TsidUtil;
+import today.seasoning.seasoning.user.domain.User;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class FriendRequest extends BaseTimeEntity {
+
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_user_id")
+    private User fromUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_user_id")
+    private User toUser;
+
+    public FriendRequest(Long id, User fromUser, User toUser) {
+        this.id = id;
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+    }
+
+    public static FriendRequest build(User fromUser, User toUser) {
+        return new FriendRequest(TsidUtil.createLong(), fromUser, toUser);
+    }
+}
---
+++ src/main/java/today/seasoning/seasoning/friendship/domain/FriendRequestRepository.java
@@ -0,0 +1,9 @@
+package today.seasoning.seasoning.friendship.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+
+}
