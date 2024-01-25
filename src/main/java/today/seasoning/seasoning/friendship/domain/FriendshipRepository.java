dc180ae73a73edc6d4b9e1d0841d888ac8df101a
csct3434 <csct3434@gmail.com>
2024-01-26 04:35:34 +0900

    refactor: 친구 수락 서비스 로직 개선
    - FriendRequest를 활용한 서비스 로직 단순화

--- src/main/java/today/seasoning/seasoning/friendship/domain/FriendshipRepository.java
+++ src/main/java/today/seasoning/seasoning/friendship/domain/FriendshipRepository.java
@@ -1,21 +1,20 @@
 package today.seasoning.seasoning.friendship.domain;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Optional;
+import today.seasoning.seasoning.user.domain.User;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
 	@Query("SELECT f FROM Friendship f WHERE f.user.id = :userId AND f.friend.id = :friendId")
-	Optional<Friendship> findByUserIds(
-		@Param("userId") Long userId,
-		@Param("friendId") Long friendId
-	);
+	Optional<Friendship> findByUserIdAndFriendId(@Param("userId") Long userId, @Param("friendId") Long friendId);
 
-	@Query("SELECT f FROM Friendship f WHERE f.user.id = :friendId")
-	List<Friendship> findByFriendId(@Param("friendId") Long friendId);
+	@Query("SELECT f.friend FROM Friendship f WHERE f.user.id = :userId")
+	List<User> findFriendsByUserId(@Param("userId") Long userId);
+
+	boolean existsByUserIdAndFriendId(Long userId, Long friendId);
 
 }
