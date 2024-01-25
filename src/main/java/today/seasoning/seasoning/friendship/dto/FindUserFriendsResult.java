35e60d79b9dc425cc8aa84676a7cff12301dcd43
csct3434 <csct3434@gmail.com>
2024-01-26 04:47:36 +0900

    refactor: 친구 목록 조회 서비스 로직 개선
    FriendRequest를 활용한 서비스 로직 단순화
    
    - FriendshipRepository.findFriendsByUserId() 반환값 변경 : Friendship -> 친구에 해당하는 User 엔티티
    - build 메서드를 추가하여 FindUserFriendsResult 객체 생성을 추상화

--- src/main/java/today/seasoning/seasoning/friendship/dto/FindUserFriendsResult.java
+++ src/main/java/today/seasoning/seasoning/friendship/dto/FindUserFriendsResult.java
@@ -2,12 +2,20 @@ package today.seasoning.seasoning.friendship.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import today.seasoning.seasoning.user.domain.User;
 
 @Getter
 @RequiredArgsConstructor
 public class FindUserFriendsResult {
 
-	private final String nickname;
-	private final String accountId;
-	private final String profileImageUrl;
+    private final String nickname;
+    private final String accountId;
+    private final String profileImageUrl;
+
+    public static FindUserFriendsResult build(User friend) {
+        return new FindUserFriendsResult(
+            friend.getNickname(),
+            friend.getAccountId(),
+            friend.getProfileImageUrl());
+    }
 }
