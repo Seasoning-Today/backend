a8ccbbd0ab89954a844c092ee3454b85de08a120
csct3434 <csct3434@gmail.com>
2024-01-26 21:00:19 +0900

    test: 단위 테스트 작성 - 친구 신청 취소 서비스

---
+++ src/test/java/today/seasoning/seasoning/friendship/service/CancelFriendRequestServiceTest.java
@@ -0,0 +1,79 @@
+package today.seasoning.seasoning.friendship.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.BDDMockito.given;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import today.seasoning.seasoning.common.enums.LoginType;
+import today.seasoning.seasoning.common.exception.CustomException;
+import today.seasoning.seasoning.friendship.domain.FriendRequestRepository;
+import today.seasoning.seasoning.user.domain.User;
+import today.seasoning.seasoning.user.domain.UserRepository;
+
+@DisplayName("친구 신청 취소 서비스")
+@ExtendWith(MockitoExtension.class)
+class CancelFriendRequestServiceTest {
+
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    FriendRequestRepository friendRequestRepository;
+    @InjectMocks
+    CancelFriendRequestService cancelFriendRequestService;
+
+    User user = new User("user", "https://test.com/requester.jpg", "requester@email.com", LoginType.KAKAO);
+    User requestee = new User("requestee", "https://test.com/requestee.jpg", "requestee@email.com", LoginType.KAKAO);
+
+    @Test
+    @DisplayName("성공")
+    void success() {
+        //given : 상대방이 조회되고, 상대로부터 나에게 온 친구 신청 내역이 존재하면
+        given(userRepository.findByAccountId(requestee.getAccountId()))
+            .willReturn(Optional.of(requestee));
+
+        given(friendRequestRepository.existsByFromUserIdAndToUserId(user.getId(), requestee.getId()))
+            .willReturn(true);
+
+        //when & then : 예외가 발생하지 않는다
+        assertDoesNotThrow(() -> cancelFriendRequestService.doService(user.getId(), requestee.getAccountId()));
+    }
+
+    @Test
+    @DisplayName("실패 - 상대방 조회 불가")
+    void failedByRequesterNotFound() {
+        //given : 아이디에 해당하는 상대방이 존재하지 않으면
+        given(userRepository.findByAccountId(requestee.getAccountId()))
+            .willReturn(Optional.empty());
+
+        //when & then : Bad Request 예외가 발생한다
+        assertFailedValidation(user.getId(), requestee.getAccountId(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    @DisplayName("실패 - 신청 내역 조회 불가")
+    void failedByFriendRequestNotFound() {
+        //given : 내가 상대방에게 친구 신청한 내역이 없으면
+        given(friendRequestRepository.existsByFromUserIdAndToUserId(user.getId(), requestee.getId()))
+            .willReturn(false);
+
+        given(userRepository.findByAccountId(requestee.getAccountId()))
+            .willReturn(Optional.of(requestee));
+
+        //when & then : Bad Request 예외가 발생한다
+        assertFailedValidation(user.getId(), requestee.getAccountId(), HttpStatus.BAD_REQUEST);
+    }
+
+    private void assertFailedValidation(Long requesterId, String requesteeAccountId, HttpStatus httpStatus) {
+        assertThatThrownBy(() -> cancelFriendRequestService.doService(requesterId, requesteeAccountId))
+            .isInstanceOf(CustomException.class)
+            .hasFieldOrPropertyWithValue("httpStatus", httpStatus);
+    }
+}
