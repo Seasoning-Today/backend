818d8d336e0950ea7ab454595eb59d32920d4aa1
csct3434 <csct3434@gmail.com>
2024-02-05 23:16:45 +0900

    refactor: 기록장 등록 이미지 업로드 로직 개선
    - MultipartFile 유효성 검증 로직 개선

--- src/main/java/today/seasoning/seasoning/article/service/RegisterArticleService.java
+++ src/main/java/today/seasoning/seasoning/article/service/RegisterArticleService.java
@@ -15,14 +15,12 @@ import today.seasoning.seasoning.article.dto.RegisterArticleCommand;
 import today.seasoning.seasoning.common.aws.S3Service;
 import today.seasoning.seasoning.common.aws.UploadFileInfo;
 import today.seasoning.seasoning.common.exception.CustomException;
-import today.seasoning.seasoning.common.util.TsidUtil;
 import today.seasoning.seasoning.solarterm.domain.SolarTerm;
 import today.seasoning.seasoning.solarterm.service.SolarTermService;
 import today.seasoning.seasoning.user.domain.User;
 import today.seasoning.seasoning.user.domain.UserRepository;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class RegisterArticleService {
 
@@ -35,45 +33,41 @@ public class RegisterArticleService {
     @Value("${ARTICLE_IMAGES_LIMIT}")
     private int ARTICLE_IMAGES_LIMIT;
 
+    @Transactional
     public Long doRegister(RegisterArticleCommand command) {
-        Article article = createArticle(command);
-        articleRepository.save(article);
+        Article article = registerArticle(command);
 
-        uploadAndRegisterArticleImages(article, command.getImages());
+        if (command.getImages() != null && !command.getImages().isEmpty()) {
+            registerArticleImages(article, command.getImages());
+        }
 
         return article.getId();
     }
 
-    private Article createArticle(RegisterArticleCommand command) {
+    private Article registerArticle(RegisterArticleCommand command) {
         User user = userRepository.findByIdOrElseThrow(command.getUserId());
 
         SolarTerm solarTerm = solarTermService.findRecordSolarTerm()
             .orElseThrow(() -> new CustomException(HttpStatus.FORBIDDEN, "등록 기간이 아닙니다."));
 
-        return new Article(user, command.isPublished(), solarTerm.getDate().getYear(),
+        Article article = new Article(user, command.isPublished(), solarTerm.getDate().getYear(),
             solarTerm.getSequence(), command.getContents());
+
+        return articleRepository.save(article);
     }
 
-    private void uploadAndRegisterArticleImages(Article article, List<MultipartFile> images) {
+    private void registerArticleImages(Article article, List<MultipartFile> images) {
         if (images.size() > ARTICLE_IMAGES_LIMIT) {
             throw new CustomException(HttpStatus.BAD_REQUEST, "이미지 개수 초과");
         }
 
-        for (int sequence = 0; sequence < images.size(); sequence++) {
-            MultipartFile image = images.get(sequence);
+        int sequence = 1;
+        for (MultipartFile image : images) {
+            if (image == null || image.isEmpty()) {
+                continue;
+            }
             UploadFileInfo fileInfo = s3Service.uploadFile(image);
-            registerArticleImage(article, fileInfo, sequence + 1);
+            articleImageRepository.save(ArticleImage.build(article, fileInfo, sequence++));
         }
-    }
-
-    private void registerArticleImage(Article article, UploadFileInfo fileInfo, int sequence) {
-        ArticleImage articleImage = new ArticleImage(
-            TsidUtil.createLong(),
-            article,
-            fileInfo.getFilename(),
-            fileInfo.getUrl(),
-            sequence);
-
-        articleImageRepository.save(articleImage);
     }
 }
