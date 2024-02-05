baa7f783ffb5eec96757e2d0fcdc0b8d41391fc3
csct3434 <csct3434@gmail.com>
2024-02-05 23:47:43 +0900

    refactor: 기록장 수정 이미지 업로드 로직 개선
    - 이미지가 변경되지 않은 경우, 이미지를 재업로드하지 않도록 개선
    - MultipartFile 유효성 검증 로직 개선

--- src/main/java/today/seasoning/seasoning/article/dto/UpdateArticleCommand.java
+++ src/main/java/today/seasoning/seasoning/article/dto/UpdateArticleCommand.java
@@ -7,14 +7,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 public class UpdateArticleCommand {
 
+	private final boolean imageModified;
 	private final Long userId;
 	private final Long articleId;
 	private final boolean published;
 	private final String contents;
 	private final List<MultipartFile> images;
 
-	public UpdateArticleCommand(Long userId, Long articleId, boolean published, String contents,
+	public UpdateArticleCommand(boolean imageModified, Long userId, Long articleId, boolean published, String contents,
 		List<MultipartFile> images) {
+		this.imageModified = imageModified;
 		this.userId = userId;
 		this.articleId = articleId;
 		this.published = published;
--- src/main/java/today/seasoning/seasoning/article/dto/UpdateArticleDto.java
+++ src/main/java/today/seasoning/seasoning/article/dto/UpdateArticleDto.java
@@ -1,12 +1,19 @@
 package today.seasoning.seasoning.article.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UpdateArticleDto {
+
+	@NotNull
+	@JsonProperty("image_modified")
+	private Boolean imageModified;
 
 	@NotNull
 	private Boolean published;
