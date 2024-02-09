85e160b6a698aa3a6dee23bdcc4eefcb43be9a8f
csct3434 <csct3434@gmail.com>
2024-02-10 04:45:12 +0900

    chore: 회원 프로필 응답 속성명 변경(profileImageUrl -> image)

--- src/main/java/today/seasoning/seasoning/user/dto/UserProfileResponse.java
+++ src/main/java/today/seasoning/seasoning/user/dto/UserProfileResponse.java
@@ -14,13 +14,13 @@ public class UserProfileResponse {
 	private String id;
 	private String nickname;
 	private String accountId;
-	private String profileImageUrl;
+	private String image;
 
-	public UserProfileResponse(Long id, String nickname, String accountId, String profileImageUrl) {
+	public UserProfileResponse(Long id, String nickname, String accountId, String image) {
 		this.id = TsidUtil.toString(id);
 		this.nickname = nickname;
 		this.accountId = accountId;
-		this.profileImageUrl = profileImageUrl;
+		this.image = image;
 	}
 
 	public static UserProfileResponse build(User user) {
