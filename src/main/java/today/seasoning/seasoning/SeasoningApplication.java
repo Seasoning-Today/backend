fadcef4223d063454fcff4f13f7b40acb0b518c3
csct3434 <csct3434@gmail.com>
2024-03-01 00:17:00 +0900

    chore: 타임존 설정 (서울)

--- src/main/java/today/seasoning/seasoning/SeasoningApplication.java
+++ src/main/java/today/seasoning/seasoning/SeasoningApplication.java
@@ -1,5 +1,7 @@
 package today.seasoning.seasoning;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +10,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SeasoningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeasoningApplication.class, args);
-	}
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasoningApplication.class, args);
+    }
 
 }
