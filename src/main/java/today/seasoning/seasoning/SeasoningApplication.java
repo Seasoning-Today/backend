5dbba051b671cc1c5cec84bf143125bcc3176494
csct3434 <csct3434@gmail.com>
2024-03-22 10:15:35 +0900

    chore: 서버 KST 타임존 수동 설정

--- src/main/java/today/seasoning/seasoning/SeasoningApplication.java
+++ src/main/java/today/seasoning/seasoning/SeasoningApplication.java
@@ -1,15 +1,26 @@
 package today.seasoning.seasoning;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class SeasoningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SeasoningApplication.class, args);
+    }
+
+    @PostConstruct
+    void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("Server Time : {}", LocalDateTime.now());
     }
 
 }
