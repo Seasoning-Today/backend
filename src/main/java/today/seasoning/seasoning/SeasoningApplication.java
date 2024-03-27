9d541bdb2a023637b6291855c574dbddb864e2e4
csct3434 <csct3434@gmail.com>
2024-03-27 19:39:10 +0900

    chore: 애플리케이션 시작 시 Datasource URL 로깅

--- src/main/java/today/seasoning/seasoning/SeasoningApplication.java
+++ src/main/java/today/seasoning/seasoning/SeasoningApplication.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SeasoningApplication {
 
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(SeasoningApplication.class, args);
     }
@@ -21,6 +25,11 @@ public class SeasoningApplication {
     void setTimeZone() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         log.info("Server Time : {}", LocalDateTime.now());
+    }
+
+    @PostConstruct
+    void logDatasourceUrl() {
+        log.info("Datasource URL : {}", dataSourceUrl);
     }
 
 }
